9a3479f435bb3855920709f485359bee3a4c9b42
Denis Valinurov <dvalinurov.yandex-team.ru>
2025-04-08 17:21:35 +0300

    iter20: add version

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/valinurovdenis/urlshortener/cmd/shortener/runner"
 )
 
+var (
+	buildVersion string = "N/A"
+	buildDate    string = "N/A"
+	buildCommit  string = "N/A"
+)
+
 func main() {
+	fmt.Printf("Build version: %s\n", buildVersion)
+	fmt.Printf("Build date: %s\n", buildDate)
+	fmt.Printf("Build commit: %s\n", buildCommit)
+
 	if err := runner.Run(); err != nil {
 		panic(err)
 	}
